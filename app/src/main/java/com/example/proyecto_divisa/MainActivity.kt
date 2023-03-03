d5218cb1d4f1a16e6ec72250a7469cfa4cc9d148
AdrianGG <adrian190400@hotmail.com>
2023-03-03 15:57:58 -0600

    Worker corregido

--- app/src/main/java/com/example/proyecto_divisa/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_divisa/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val constraints = Constraints.Builder()
             .setRequiredNetworkType(NetworkType.UNMETERED) //TODO: Revisar si es el tipo adecuado
             .build()
--- app/src/main/java/com/example/proyecto_divisa/work_manager/InsertarDivisaWorker.kt
+++ app/src/main/java/com/example/proyecto_divisa/work_manager/InsertarDivisaWorker.kt
@@ -4,25 +4,24 @@ import android.content.Context
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.room.Room
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.proyecto_divisa.Aplicacion
 import com.example.proyecto_divisa.db.DivisaDatabase
 import com.example.proyecto_divisa.model.Divisa
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 
 class InsertarDivisaWorker(appContext: Context, workerParams: WorkerParameters):
-    Worker(appContext, workerParams) {
+    CoroutineWorker(appContext, workerParams) {
     lateinit var bd : DivisaDatabase
     @RequiresApi(Build.VERSION_CODES.O)
-    override fun doWork(): Result{
+    override suspend fun doWork(): Result{
         return try {
-            var d = Divisa(0, "USD", "MXN", 18.1, LocalDateTime.now().toString())
             bd = (applicationContext as Aplicacion).database
-            GlobalScope.launch { bd.getDivisaDAO().insertar(d) }
+            /*FOR*/
+            var d = Divisa(0, "USD", "MXN", 18.1, LocalDateTime.now().toString())
+            bd.getDivisaDAO().insertar(d)
+            /*FOR*/
             Result.success()
         } catch (throwable: Throwable) {
             Log.e("", "Error al insertar")
